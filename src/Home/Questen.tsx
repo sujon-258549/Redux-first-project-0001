37fabbe9fa32fbd8d41dc4a819252c3636964f0d
Md Sujon Mia <sujon258549@gmail.com>
2025-01-07 23:07:40 +0600

    modified App.tsx and Questen.tsx

---
+++ src/Home/Questen.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { BellRing, Check } from "lucide-react";
+
+const Questen = () => {
+  return (
+    <div>
+      <Card className={cn("w-[380px]")}>
+        <CardHeader>
+          <CardTitle>Notifications</CardTitle>
+          <CardDescription>You have 3 unread messages.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className=" flex items-center space-x-4 rounded-md border p-4">
+            <BellRing />
+            <div className="flex-1 space-y-1">
+              <p className="text-sm font-medium leading-none">
+                Push Notifications
+              </p>
+              <p className="text-sm text-muted-foreground">
+                Send notifications to device.
+              </p>
+            </div>
+            <switch />
+          </div>
+          <div></div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">
+            <Check /> Mark all as read
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Questen;
