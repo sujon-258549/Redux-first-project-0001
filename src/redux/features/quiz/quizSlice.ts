03e171dbf087b7c31898ffc04822c0a29d72d445
Md Sujon Mia <sujon258549@gmail.com>
2025-01-08 10:54:53 +0600

    modified main.tsx added quizSlice.ts

---
+++ src/redux/features/quiz/quizSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../app/store";
+
+const quizSlice = createSlice({
+  name: "quize",
+  initialState: {},
+  reducers: {},
+});
+
+export default quizSlice;
