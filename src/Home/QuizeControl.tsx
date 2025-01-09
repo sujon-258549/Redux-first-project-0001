a3d539a524192e7b428b236b326ed0daa0a08ead
Md Sujon Mia <sujon258549@gmail.com>
2025-01-09 23:03:12 +0600

    modified QuizeControl.tsx in Home

--- src/Home/QuizeControl.tsx
+++ src/Home/QuizeControl.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { nextQuestion, prevQuestion } from "@/redux/features/quiz/quizSlice";
-import { useAppDispatch } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 // import { CardFooter } from "@/components/ui/card";
 
 const QuizControl = () => {
+  const { question, currentIndexForQuiz, userAnswer } = useAppSelector(
+    (select) => select.quiz
+  );
+  const isExistNotSelectQuestion = userAnswer[currentIndexForQuiz] === null;
   const dispatch = useAppDispatch();
   const handelPrevious = () => {
     dispatch(prevQuestion());
@@ -14,12 +18,27 @@ const QuizControl = () => {
   return (
     // <CardFooter>
     <div className="flex justify-between px-6 pb-5">
-      <Button onClick={handelPrevious} className="">
+      <Button
+        disabled={currentIndexForQuiz === 0}
+        onClick={handelPrevious}
+        className=""
+      >
         Pre
       </Button>
-      <Button onClick={handledNext} className="">
-        Next
-      </Button>
+      {currentIndexForQuiz < question.length - 1 && (
+        <Button
+          disabled={isExistNotSelectQuestion}
+          onClick={handledNext}
+          className=""
+        >
+          Next
+        </Button>
+      )}
+      {currentIndexForQuiz === question.length - 1 && (
+        <Button onClick={handledNext} className="">
+          Completed quiz
+        </Button>
+      )}
     </div>
     // </CardFooter>
   );
